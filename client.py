d4727eaa6d00d47e5d277233c2fc3ee791f84e7b
luisfernandoam10 <luisfernandoam10@hotmail.com>
2018-06-15 18:57:46 -0300

    Almost done.

--- client.py
+++ client.py
@@ -1,4 +1,7 @@
 import socket
+import os, sys
+import zipfile,shutil
+
 HOST = '127.0.0.1'     # Endereco IP do Servidor
 PORT = 5000            # Porta que o Servidor esta
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,23 +24,23 @@ def signup(): #def signup(users):
 	print "Register:\n"
 	username=raw_input('Insert your username: ')
 	pwd=raw_input('Insert your password: ')
-	tcp.send (mode+" "+username+" "+pwd+"\n")
+	tcp.send (mode+" "+username+" "+pwd)
 	print "Successful\n"
 
 def signin(): #def signin(users):
 	mode='2'
-	pwd='-'
 	print "Login:\n"
 	username=raw_input('Insert your username: ')
-	tcp.send(mode+" "+username+" "+pwd+"\n")
+	tcp.send(mode+" "+username)
 	msg = tcp.recv(1024)
+	print "msg: ",msg
 	if msg=="ask_pwd":
 		msg=raw_input("Insert your password: ")
 		tcp.send(msg)
 		msg = tcp.recv(1024)
 		if msg=="signin_ok":
 			print "Login successful.\n"
-			return 1
+			return 1,username
 		else:
 			print "Wrong password.\n"
 	elif msg=="db_nf":
@@ -46,26 +49,209 @@ def signin(): #def signin(users):
 		print "Username not found.\n"
 	return 0
 
-
-
+def ls():
+	i=0
+	tcp.send("ls")
+	files = tcp.recv(32)
+	while not files.endswith("\0"): files += tcp.recv(32)
+	print files
+	return 1
 
 # main
 mode=start_menu()
-op='0'
-dire='/home'
 while mode <> 'exit':
+	op='0'
+	path="/"
 	if mode=="1":
 		signup()
 	elif mode=="2":
 		logged=signin()
-		help()
-		while( logged and (op<>'logout') ):
-			
-			op=raw_input(dire+"> ")
-			if op=='help':
+		print "logged: ",logged
+		if (op[0]=='logout'):
+			tcp.send("logout")
+		if logged:
+			path=logged[1]
+			rpath=path
+		while( logged and (op[0]<>'logout') ):			
+			ppath=path #previous path
+			print "path: ",path
+			# if not os.path.exists(path):
+			# 	os.makedirs(path)
+			op=raw_input(path+"> ")
+			op = op.split()
+			if len(op)==0:
+				op=' '
+				continue
+			if op[0]=='logout':
+				print op[0]
+				tcp.send("logout")
+			if op[0]=='help':
 				help()
+			elif op[0]=='cd':
+				if len(op)==1:
+					print "Invalid operand."
+					continue
+				elif op[1]=='..':
+					if path==rpath:
+						print "Can't."
+						continue
+				tcp.send(op[0]+" "+op[1])
+				msg = tcp.recv(5)
+				if msg=='cd_nf':
+					print "Directory not found."
+				elif msg=='cd_ok':
+					path=os.path.join(path,op[1])
+					print "Cd Success."
+				elif msg=='cd2ok':
+					path=os.path.normpath(path+os.sep+os.pardir) #retrocede uma pasta
+					print "Cd.. Success."
+				else: #cd2no
+					pass
+			elif op[0]=='ls':
+				ls()
+			elif op[0]=='mkdir':
+				if len(op)==1:
+					print "Invalid operand."
+					continue
+				tcp.send(op[0]+" "+op[1])
+				msg = tcp.recv(8)
+				if msg=='mkdir_ae':
+					print "Already exists."
+				else:
+					print "Created."
+			elif op[0]=='mv':
+				if len(op)<3:
+					print "Invalid operand."
+					continue
+				tcp.send(op[0]+" "+op[1]+" "+op[2])
+				msg = tcp.recv(5)
+				if msg=='mv_ok':
+					print "Moved."
+				else:
+					print "Error."
+			elif op[0]=='rm':
+				if len(op)==1:
+					print "Invalid operand."
+					continue
+				tcp.send(op[0]+" "+op[1])
+				msg = tcp.recv(5)
+				if msg=='rm_ok':
+					print "Removed."
+				else:
+					print "No such file or directory."
+
+			elif op[0]=='upload':
+				if len(op)==1:
+					print "Invalid operand."
+					continue
+				tcp.send(op[0]+" "+op[1])
+
+				print "atual: ",os.getcwd()
+
+				root_dir = os.path.normpath(op[1]+os.sep+os.pardir)
+				base_dir = os.path.relpath(op[1],root_dir)
+				##base_dir=os.path.basename(path) retorna nome arq/pasta
+				try:
+					shutil.make_archive('temp','zip',root_dir,base_dir)
+
+
+					fsize = os.path.getsize('temp.zip')
+					print "fsize: ",str(fsize)
+					tcp.send(str(fsize)) # envia tamanho
+					msg=tcp.recv(2)
+					if msg=='ok': # se autorizado a enviar, envia
+						with open('temp.zip','rb') as fs:
+							data = fs.read(1024)
+							while data:
+								tcp.send(data)
+								data = fs.read(1024)
+						print "Upload completed."
+					else: print "Error."
+
+					os.remove('temp.zip')
+
+				except OSError:
+					print "File or directory not found."
+					continue
+
+
+			elif op[0]=='download':
+				if len(op)==1:
+					print "Invalid operand."
+					continue
+				tcp.send(op[0]+" "+op[1])
+
+				print "atual: ",os.getcwd()
+
+				with open('temp.zip', 'wb') as fw:
+					msgtam = tcp.recv(1024) #recebe tam arquivo
+					fsize = int(msgtam)
+					print "fsize: ",fsize
+					rsize = 0
+					tcp.send("ok") #autoriza receber
+					while True:
+						data = tcp.recv(1024)
+						rsize += len(data)
+						fw.write(data)
+						if  rsize >= fsize:
+							break
+					print "Download Completed"
+				msg = tcp.recv(7)
+				print "msg_down: ",msg
+
+				with zipfile.ZipFile('temp.zip',"r") as zip_ref:
+					zip_ref.extractall()
+
+				os.remove('temp.zip')
+
+				if msg=='down_ok':
+					print "Sucess."
+				else:
+					print "File or directory not found."
+
+
+			else: print "Invalid operand. See help."
+
+
 	mode=start_menu()
 
 
 tcp.send("fin")
 tcp.close()
+
+# base_dir = '/bla/bing'
+# filename = 'data.txt'
+# os.path.join(base_dir, filename)
+
+
+
+
+# while True:
+#     data = s.recv(1024)
+
+#     if not data: print " Done "; break
+
+#     recvd += data
+
+
+
+# import shutil
+# file_to_zip = 'test.txt'            # file to zip
+# target_path = 'C:\\test_yard\\'     # dir, where file is
+
+# try:
+#     shutil.make_archive('archive', 'zip', target_path, file_to_zip)
+# except OSError:
+#     pass
+
+
+
+# shutil.make_archive('nome','zip','/home/luisfernando/Documents/TD/trab1/','diretorio')
+
+
+					# saved_path = os.getcwd()
+					# os.chdir(new_path)
+					# try:
+					#     # code that does stuff in new_path goes here
+					# finally:
+					#     os.chdir(saved_path)
--- server.py
+++ server.py
@@ -1,19 +1,35 @@
-import socket
-import thread
+import socket, thread
+import os, sys
+from subprocess import Popen, PIPE
+import subprocess
+import zipfile,shutil
 
 HOST = ''              # Endereco IP do Servidor
 PORT = 5000            # Porta que o Servidor esta
 
 users = {}
 
-def decode(msg):
-    (mode,username,pwd) = msg.split()
-    return mode,username,pwd
+#protocolo: cmd, op1, op2
+#cmd eh comando (1 eh cadastro, 2 eh login ...)
+#op1 pode ser: usuario, origem
+#op2 pode ser: senha, destino
 
-def signup(user,pwd): #def signup(users):
+def decode(msg):
+    receive = msg.split()
+    #(cmd,op1,op2) = msg.split()
+    if len(receive)==1:
+        print "decode.tamanho 1"
+        return receive[0]
+    elif len(receive)==2:
+        return receive[0],receive[1]
+
+    print "decode.tamanho 3"
+    return receive[0],receive[1],receive[2]
+
+def signup(receive): #def signup(users):
 
     with open("bd.txt","a") as f:
-        f.write(user+" "+pwd+"\n")
+        f.write(receive[1]+" "+receive[2]+"\n")
     #print 'sucesso\n'
     return 1
 
@@ -25,10 +41,9 @@ def signin(con,user): #def signin(users):
         con.send("db_nf")
     else:
         for line in f:
-            (username, pwd) = line.split()
-            users[username] = pwd
+            (op1, op2) = line.split()
+            users[op1] = op2
         f.close()
-    
     if user in users:
         con.send("ask_pwd")
         #key = raw_input('Informe a senha: ')
@@ -39,28 +54,236 @@ def signin(con,user): #def signin(users):
         else:
             con.send("wrong_pwd")
             return 0
+    con.send("user_nf")
     return -1
 
+def console(cmd):
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    out, err = p.communicate()
+    return (p.returncode, out, err)
 
 def conectado(con, cliente):
     print 'A connection was successfully established with', cliente
 
     while True:
-        #print "oi"
+        print "\n\nInicio"
         msg = con.recv(1024)
         if not msg: break
         if msg=="fin": break
         print cliente, msg
         receive = decode(msg)
-        # print "<DEBUG> mode: ",mode
-        # print "<DEBUG> username: ",user,", pwd: ",pwd
+        print "receive1: ",receive
+
         if receive[0]=="1":
-            mode,user,pwd = decode(msg)
-            signup(user,pwd)
+            signup(receive)
         elif receive[0]=="2":
-            mode,user,pwd = decode(msg)
-            if(signin(con,user)==-1):
-                con.send("user_nf") #user not found
+            if(signin(con,receive[1])<>1):
+                print "Usuario nao logou"
+                continue
+
+
+
+# >>> rpath#### isso sera o rpath= os.getcwd
+# '/home/luisfernando/Documents/TD/trab1/servidor'
+# >>> relpath
+# 'luis/aa'
+# >>> os.path.join(rpath,relpath)
+# '/home/luisfernando/Documents/TD/trab1/servidor/luis/aa'
+
+
+# IMPORTANTE: FAZER NUNCA ENTRAR EM NENHUMA PASTA. FAZER RPATH SER
+# ONDE ESTA O SERVER.PY
+# FAZER FUNCIONAR DIRETORIO RELATIVO E ABSOLUTO
+# DICA: FAZER EQUIVALENCIA DE DIRETORIO SEMPRE E USAR SEMPRE O ABSOLUTO
+# O QUE PENSEI: EU SEMPRE SEI O ENDERECO RELATIVO (/luis/aa etc)
+# TAMBEM SEMPRE SEI O RPATH
+# CASO O USUARIO SEMPRE DER O ABSOLUTO (lado cliente), seria so fazer join
+# como tambem eh pra aceitar relativo, verificar se eh relativo
+# se relativo, juntar rpath+root_dir+end_relativo
+# root_dir eh a subtracao (path - relativo) e rpath
+#>>parentpath=os.path.normpath(path+os.sep+os.pardir)
+#>>root_dir=os.path.relpath(parentpath,rpath)
+
+# ex: path eh /servidor/luis/aa e rpath eh /servidor
+# root_dir = /luis
+# root_dir seria o os.path.relpath(path,rpath)
+# se o usuario mandar relativo, e eu tenho path, so fazer o join dos 3
+# se o usuario mandar absotulo
+
+            rpath=os.getcwd() #root path
+            path=os.path.join(rpath,receive[1]) #user path
+            relpath=receive[1]
+
+            #print "path: ",path
+            if not os.path.exists(path):
+                os.makedirs(path)
+            #os.chdir(upath)
+            msg=con.recv(1024)
+            receive = decode(msg)
+            print "receive2: ",receive
+            while receive<>'logout':
+                #print 'inicio.path: ',path
+                if receive=="ls":
+                    print "ls.path: ",path
+                    files = [f for f in os.listdir(path)]
+                    #print "files: ",files
+                    #print "f: ",f
+                    for f in files:
+                        print f
+                        con.send(f+"   ")
+                    con.send('\0') #end of transmission
+                if receive[0]=="mkdir":
+                    ppath=path
+                    path=os.path.join(ppath,receive[1])
+                    print "mkdir.path: ",path
+                    if not os.path.exists(path):
+                        os.mkdir(path)
+                        con.send("mkdir_ok")
+                    else: con.send("mkdir_ae") #already exists
+                    path=ppath
+                if receive[0]=="mv":
+                    print "path: ",path
+                    print "atual: ",os.getcwd()
+                    print "receive[2]: ",receive[2]
+                    print "rpath+receive[2]: ",rpath+receive[2]
+                    cmd = receive[0]+" "+receive[1]+" "+receive[2]
+                    if receive[2][0]=='/':
+                        dire=rpath+receive[2]
+                        cmd = receive[0]+" "+receive[1]+" "+dire
+                    print cmd
+                    os.chdir(path)
+
+                    op = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+                    output, err = op.communicate()
+                    if not err:
+                        #output=str(op.stdout.read())
+                        print "Output:",output
+                        con.send('mv_ok')
+                    else:
+                        print "Error:",err
+                        con.send('mv_no')
+                    os.chdir(os.path.normpath(path+os.sep+os.pardir)) #retrocede uma pasta
+                if receive[0]=="rm":
+                    print "path: ",path
+                    print "atual: ",os.getcwd()
+                    cmd = receive[0]+" -rf "+receive[1]
+                    print cmd
+                    os.chdir(path)
+
+                    op = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+                    output, err = op.communicate()
+                    if not err:
+                        #output=str(op.stdout.read())
+                        print "Output:",output
+                        con.send('rm_ok')
+                    else:
+                        print "Error:",err
+                        con.send('rm_no')
+                    os.chdir(os.path.normpath(path+os.sep+os.pardir)) #retrocede uma pasta
+
+                if receive[0]=="cd":
+                    ppath=path
+                    print "cd.path: ",path
+                    print "atual: ",os.getcwd()
+                    if receive[1]=='..': #cd ..
+                        path=os.path.normpath(path+os.sep+os.pardir)
+                        print "cd.. .path: ",path
+                        if os.path.exists(path):
+                            #os.chdir("..");
+                            print "cd.path2: ",path
+                            con.send("cd2ok")
+                        else: 
+                            con.send("cd2no")
+                            path=ppath
+                            print "cd.path2: ",path
+                    else:
+
+                        path=os.path.join(ppath,receive[1])
+                        if os.path.isdir(path):
+                            #os.chdir(path)
+                            con.send("cd_ok")
+                        else:
+                            path=ppath
+                            con.send("cd_nf")
+
+                if receive[0]=="upload":
+
+                    print "atual: ",os.getcwd()
+                    print "receive[1]: ",receive[1]
+
+
+                    # temp = os.path.relpath(receive[1],os.path.normpath(receive[1]+os.sep+os.pardir))
+                    # print "temp: ",temp
+                    # temp+='.zip'
+                    # print temp
+
+                    with open('temp.zip', 'wb') as fw:
+                        msgtam = con.recv(1024) #recebe tam arquivo
+                        fsize = int(msgtam)
+                        print "fsize: ",fsize
+                        rsize = 0
+                        con.send("ok") #autoriza enviar
+                        while True:
+                            data = con.recv(1024)
+                            rsize += len(data)
+                            fw.write(data)
+                            if  rsize >= fsize:
+                                break
+                    print "Upload Completed"
+
+                    with zipfile.ZipFile('temp.zip',"r") as zip_ref:
+                        zip_ref.extractall()
+
+                    os.remove('temp.zip')
+                    # sData = con.recv(1024)
+                    # print "recebi sData: ",sData
+                    # with open(receive[1],"wb") as fDownloadFile:
+                    #     print "criei arquivo"
+                    #     while sData:
+                    #         fDownloadFile.write(sData)
+                    #         print "vou receber"
+                    #         sData = con.recv(1024)
+                    #         print "recebi"
+                    # print "Download Completed"
+
+                if receive[0]=="download":
+
+                    print "down.atual: ",os.getcwd()
+
+
+                    root_dir = os.path.normpath(receive[1]+os.sep+os.pardir)
+                    base_dir = os.path.relpath(receive[1],root_dir)
+
+                    try:
+                        shutil.make_archive('temp','zip',root_dir,base_dir)
+
+                        fsize = os.path.getsize('temp.zip')
+                        print "fsize: ",str(fsize)
+                        con.send(str(fsize)) # envia tamanho
+                        msg=con.recv(2)
+                        if msg=='ok': # se autorizado a receber, envia
+                            with open('temp.zip','rb') as fs:
+                                data = fs.read(1024)
+                                while data:
+                                    con.send(data)
+                                    data = fs.read(1024)
+                            print "Download completed."
+                            con.send('down_ok')
+                        else: print "Error."
+
+                        os.remove('temp.zip')
+
+                    except OSError:
+                        print "File or directory not found."
+                        con.send("down_nf")
+                        continue
+
+                msg=con.recv(1024)
+                receive = decode(msg)
+                print "receive3: ",receive
+            print "user logout"
+            os.chdir(rpath)
+            continue
 
     print 'Ending client connection', cliente
     con.close()
